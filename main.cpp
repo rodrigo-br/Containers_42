e649e97a2e75514ee9f980a45ec69f5bb46f80f8
rodrigo-br <rodrigoab123@gmail.com>
2022-12-07 22:13:05 -0300

    f

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include "vector.hpp"
 
+void print_line() {
+	std::cout << std::string(42, '-') << std::endl;
+}
+
+template <typename T>
+void printVector(const ft::vector<T>&vector) {
+	for (size_t i = 0; i < vector.size(); i++) {
+		std::cout << vector[i] << "\n";
+	}
+
+	print_line();
+}
+
 int main(void)
 {
 	ft::vector<std::string> vector;
@@ -8,7 +21,5 @@ int main(void)
 	vector.push_back("na");
 	vector.push_back("chuva");
 
-	for (size_t i = 0; i < vector.size(); i++) {
-		std::cout << vector[i] << std::endl;
-	}
+	printVector(vector);
 }
